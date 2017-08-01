4e28c2ca74bafe2df3fb64c01f820dade2ff0964
Ronan Fitzpatrick <xronanfitzx@gmail.com>
2017-08-01 12:31:20 -0700

    Initial Schema refactor

--- src/models/Orgs.js
+++ src/models/Orgs.js
@@ -1,69 +1,15 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 const fetch = require('isomorphic-fetch');
-const Org = require('./Org');
 
-function getJson(response) {
-  return response.json();
-}
-
-function addToArray(value) {
-  return [value];
-}
-
-function filterRelevantFields(orgs) {
-  return orgs.filter(({ id, login, url, repos_url, avatar_url }) => ({
-    id,
-    login,
-    url,
-    repos_url,
-    avatar_url,
-  }));
-}
-
-function createOrgInstances(orgs) {
-  return orgs.map(org => new Org(org));
-}
-
-class Orgs {
-  constructor(userName, orgName) {
-    this.orgName = orgName;
-    this.userOrgsUrl = `https://api.github.com/users/${userName}/orgs?access_token=${process.env.TKN}`;
-    this.userOrgUrl = `https://api.github.com/orgs/${orgName}?access_token=${process.env.TKN}`;
+class Org {
+  constructor(url) {
+    this.url = url;
   }
 
-  get orgs() {
-    return this.orgName ? this.getOrg() : this.getAllUserOrgs();
-  }
-
-  getAllUserOrgs() {
-    return fetch(this.userOrgsUrl)
-      .then(getJson)
-      .then(filterRelevantFields)
-      .then(createOrgInstances);
-  }
-
-  getOrg() {
-    return fetch(this.userOrgUrl)
-      .then(getJson)
-      .then(addToArray)
-      .then(filterRelevantFields)
-      .then(createOrgInstances);
+  async getOrgs() {
+    return fetch(this.url)
+      .then(response => response.json())
+      .then(response => response);
   }
 }
 
-module.exports = Orgs;
-=======
-=======
-
-
->>>>>>> Add formatting
-// class Orgs {
-//   constructor() {
-//     'https://api.github.com';
-//   }
-//   async fetchOrgs(url) {
-//     const userOrgs = await fetch(`${url}`).json();
-//   }
-// }
->>>>>>> Add formatting
+module.exports = Org;
