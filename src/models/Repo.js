2294ab3c0a7c4e3255196d3e75e3c7150f63d183
Ronan Fitzpatrick <xronanfitzx@gmail.com>
2017-08-02 13:17:01 -0700

    Add fetchPullRequest to repo

--- src/models/Repo.js
+++ src/models/Repo.js
@@ -1,5 +1,6 @@
 const fetch = require('isomorphic-fetch');
 const Issue = require('./Issue');
+const PullRequest = require('./PullRequest');
 
 class Repo {
   constructor(repo) {
@@ -20,6 +21,19 @@ class Repo {
       .then(response => response.map((issue) => {
         const repoIssue = new Issue(issue);
         return repoIssue;
+      }));
+  }
+
+  async fetchPullRequest() {
+    return fetch(`${this.repo.pulls_url.slice(0, -9)}?access_token=${process.env.TKN}`)
+      .then(response => response.json())
+      .then(response => response.map(pullRequests => fetch(`${pullRequests.url}?access_token=${process.env.TKN}`)))
+      .then(promiseArray => Promise.all(promiseArray))
+      .then(responses => responses.map(response => response.json()))
+      .then(responses => Promise.all(responses))
+      .then(pullRequests => pullRequests.map((pullRequest) => {
+        const pr = new PullRequest(pullRequest);
+        return pr;
       }));
   }
 }
