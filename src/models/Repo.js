3bc04c1be0969d23ac434ccf7ebbd96b3dd5f7d4
Ronan Fitzpatrick <xronanfitzx@gmail.com>
2017-08-02 11:08:59 -0700

    Complete Repo Resolver and Model

--- src/models/Repo.js
+++ src/models/Repo.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* eslint-disable camelcase, no-prototype-builtins */
 const fetch = require('isomorphic-fetch');
 const Issue = require('./Issue');
@@ -88,6 +89,31 @@ class Repo {
     return fetchData(this.milestonesUrl)
       .then(getJson)
       .then(createMilestoneInstances);
+=======
+const fetch = require('isomorphic-fetch');
+const Issue = require('./Issue');
+
+class Repo {
+  constructor(repo) {
+    this.repo = repo;
+  }
+
+  name() {
+    return this.repo.name;
+  }
+
+  id() {
+    return this.repo.id;
+  }
+
+  fetchIssue() {
+    return fetch(`${this.repo.issues_url.slice(0, -9)}?state=all&access_token=${process.env.TKN}`)
+      .then(response => response.json())
+      .then(response => response.map((issue) => {
+        const repoIssue = new Issue(issue);
+        return repoIssue;
+      }));
+>>>>>>> Complete Repo Resolver and Model
   }
 }
 
