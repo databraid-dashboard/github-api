213daccbec5581c893b4fc0846fab7ff058334ed
Ronan Fitzpatrick <xronanfitzx@gmail.com>
2017-08-03 17:41:25 -0700

    Refactor root resolver test for new variable names

--- src/resolvers/GitDataResolver.js
+++
@@ -1,9 +0,0 @@
-const User = require('../models/User');
-
-const root = {
-  getGitData(username) {
-    return new User(username);
-  },
-};
-
-module.exports = root;
--- src/resolvers/RootResolver.js
+++ src/resolvers/RootResolver.js
@@ -1,18 +1,8 @@
 const User = require('../models/User');
-const Orgs = require('../models/Orgs');
-const Repos = require('../models/Repos');
 
 const root = {
-  user(userName) {
-    return new User(userName);
-  },
-
-  orgs({ userName, orgName }) {
-    return new Orgs(userName, orgName);
-  },
-
-  repos({ userName, repoName, orgName }) {
-    return new Repos(userName, repoName, orgName);
+  getGitData(username) {
+    return new User(username);
   },
 };
 
