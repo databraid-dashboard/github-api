bb8b6cae6985ad05019ae006731339b8b79cf362
Ronan Fitzpatrick <xronanfitzx@gmail.com>
2017-08-07 14:43:02 -0700

    Refactor Repos and Repo to match new schema layout

--- src/models/Repo.js
+++ src/models/Repo.js
@@ -1,61 +1,93 @@
+/* eslint-disable camelcase, no-prototype-builtins */
 const fetch = require('isomorphic-fetch');
 const Issue = require('./Issue');
 const PullRequest = require('./PullRequest');
 const Milestone = require('./Milestone');
 
-class Repo {
-  constructor(repo) {
-    this.repo = repo;
-    this.name = this.repo.name;
-    this.id = this.repo.id;
-    this.openIssues = this.repo.open_issues;
+function fetchData(url, all) {
+  if (all) {
+    return fetch(`${url.slice(0, -9)}?state=all&access_token=${process.env.TKN}`);
   }
-  get issue() {
-    return fetch(
-      `${this.repo.issues_url.slice(0, -9)}?state=all&access_token=${process.env
-        .TKN}`,
-    )
-      .then(response => response.json())
-      .then(response =>
-        response.map((issue) => {
-          const repoIssue = new Issue(issue);
-          return repoIssue;
-        }),
-      );
+  return fetch(`${url.slice(0, -9)}?access_token=${process.env.TKN}`);
+}
+
+function getJson(response) {
+  return response.json();
+}
+
+function filterRelevantFields(issues) {
+  return issues.filter(({ id, title, labels, state, number, assignee, assigneeAvatar }) => ({
+    id,
+    title,
+    labels,
+    state,
+    number,
+    assignee,
+    assigneeAvatar,
+  }));
+}
+
+function sortByState(data) {
+  if (data.length === 0) {
+    return data;
   }
 
-  get pullRequest() {
-    return fetch(
-      `${this.repo.pulls_url.slice(0, -9)}?access_token=${process.env.TKN}`,
-    )
-      .then(response => response.json())
+  if (data[0].hasOwnProperty('pullRequest')) {
+    return data.filter(pr => pr.pullRequest.state === 'open');
+  } else if (data[0].hasOwnProperty('issue')) {
+    return data.filter(issue => issue.issue.state === 'open');
+  }
+  return data;
+}
+
+function createIssueInstances(issues) {
+  return issues.map(issue => new Issue(issue));
+}
+
+function createPullRequestInstances(pullRequests) {
+  return pullRequests.map(pullRequest => new PullRequest(pullRequest));
+}
+
+function createMilestoneInstances(milestones) {
+  return milestones.map(milestone => new Milestone(milestone));
+}
+
+class Repo {
+  constructor({ id, name, open_issues, issues_url, pulls_url, milestones_url }) {
+    this.id = id;
+    this.name = name;
+    this.openIssues = open_issues;
+    this.issuesUrl = issues_url;
+    this.pullRequestUrl = pulls_url;
+    this.milestonesUrl = milestones_url;
+  }
+
+  get issues() {
+    return fetchData(this.issuesUrl, true)
+      .then(getJson)
+      .then(filterRelevantFields)
+      .then(createIssueInstances)
+      .then(sortByState);
+  }
+
+  get pullRequests() {
+    return fetchData(this.pullRequestUrl)
+      .then(getJson)
       .then(response =>
-        response.map(pullRequests =>
-          fetch(`${pullRequests.url}?access_token=${process.env.TKN}`),
+        response.map(pullRequests => fetch(`${pullRequests.url}?access_token=${process.env.TKN}`),
         ),
       )
       .then(promiseArray => Promise.all(promiseArray))
       .then(responses => responses.map(response => response.json()))
       .then(responses => Promise.all(responses))
-      .then(pullRequests =>
-        pullRequests.map((pullRequest) => {
-          const pr = new PullRequest(pullRequest);
-          return pr;
-        }),
-      );
+      .then(createPullRequestInstances)
+      .then(sortByState);
   }
 
-  get milestone() {
-    return fetch(
-      `${this.repo.milestones_url.slice(0, -9)}?access_token=${process.env.TKN}`,
-    )
-      .then(response => response.json())
-      .then(response =>
-        response.map((milestone) => {
-          const repoMilestone = new Milestone(milestone);
-          return repoMilestone;
-        }),
-      );
+  get milestones() {
+    return fetchData(this.milestonesUrl)
+      .then(getJson)
+      .then(createMilestoneInstances);
   }
 }
 
