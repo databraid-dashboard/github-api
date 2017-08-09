8f2c172d8874784452b0fb4bb3bb4294f2f8bbc7
Ronan Fitzpatrick <xronanfitzx@gmail.com>
2017-08-09 14:22:47 -0700

    Rebase merge conflicts

--- src/models/Issue.js
+++ src/models/Issue.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Refactor User and Issue model to match new schema layout
 /* eslint-disable no-param-reassign */
-const Label = require('./Label');
-
 function getAssignee(assignee) {
   if (assignee === null) {
     assignee = 'Not assigned';
@@ -21,20 +15,6 @@ function getAssigneeAvatar(assignee) {
   return assignee.avatar_url;
 }
 
-<<<<<<< HEAD
-class Issue {
-  constructor(issue) {
-    this.issue = issue;
-    this.id = this.issue.id;
-    this.title = this.issue.title;
-    this.labels = this.issue.labels;
-    this.state = this.issue.state;
-    this.number = this.issue.number;
-    this.assignee = getAssignee(this.issue.assignee);
-    this.assigneeAvatar = getAssigneeAvatar(this.issue.assignee);
-=======
-=======
->>>>>>> Refactor User and Issue model to match new schema layout
 class Issue {
   constructor(issue) {
     this.issue = issue;
@@ -46,29 +26,6 @@ class Issue {
     this.assignee = getAssignee(this.issue.assignee);
     this.assigneeAvatar = getAssigneeAvatar(this.issue.assignee);
   }
-<<<<<<< HEAD
-
-  labels() {
-    return this.issue.labels;
-  }
-
-  state() {
-    return this.issue.state;
-  }
-
-  number() {
-    return this.issue.number;
->>>>>>> Initial issues model
-  }
-<<<<<<< HEAD
-=======
->>>>>>> Refactor model format
-=======
-
-  get label() {
-    return this.labels.map(label => new Label(label));
-  }
->>>>>>> Add Label model
 }
 
 module.exports = Issue;
--- src/models/Milestone.js
+++ src/models/Milestone.js
@@ -1,41 +1,11 @@
 class Milestone {
   constructor(milestone) {
     this.milestone = milestone;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Refactor model format
     this.id = this.milestone.id;
     this.title = this.milestone.title;
     this.openIssues = this.milestone.open_issues;
     this.closedIssues = this.milestone.closed_issues;
     this.dueOn = this.milestone.due_on;
-<<<<<<< HEAD
-=======
-  }
-
-  id() {
-    return this.milestone.id;
-  }
-
-  title() {
-    return this.milestone.title;
-  }
-
-  openIssues() {
-    return this.milestone.open_issues;
-  }
-
-  closedIssues() {
-    return this.milestone.closed_issues;
-  }
-
-
-  dueOn() {
-    return this.milestone.due_on;
->>>>>>> Complete milestone model and resolver
-=======
->>>>>>> Refactor model format
   }
 }
 
