fbd7ee0ea862034a4b407f2790391e99e7683509
Grant Willison <emailgdw@gmail.com>
2017-08-09 14:04:55 -0700

    Completed Oauth, must lint code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const schema = require('./src/schema/schema');
 const GitHubStrategy = require('passport-github2').Strategy;
 const passport = require('passport');
 const partials = require('express-partials');
-const util = require('util');
+// const util = require('util');
 const session = require('express-session');
 const methodOverride = require('method-override');
 
@@ -23,7 +23,7 @@ const allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-  if (req.method == 'OPTIONS') {
+  if (req.method === 'OPTIONS') {
     res.send(200);
   } else {
     next();
@@ -32,13 +32,13 @@ const allowCrossDomain = function (req, res, next) {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(allowCrossDomain)
+app.use(allowCrossDomain);
 
 app.use(session({
-  secret:  'keyboard cat',
+  secret: 'keyboard cat',
   keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2],
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
 }));
 
 app.use(passport.initialize());
@@ -62,23 +62,28 @@ app.use(passport.session());
 //   }));
 
 
-  passport.use(new GitHubStrategy({
-      clientID: GITHUB_CLIENT_ID,
-      clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:8000/auth/github/callback"
-    },
-    function(accessToken, refreshToken, profile, done) {
-      // asynchronous verification, for effect...
-      process.nextTick(function () {
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://localhost:8000/auth/github/callback',
+},
+  ((accessToken, refreshToken, profile, done) => {
+  // asynchronous verification, for effect...
+  console.log(process.env.TKN);
+  console.log(accessToken, 'accessToken');
+  console.log(refreshToken, 'refreshToken');
+  process.env.TKN = accessToken;
+  console.log(process.env.TKN);
+    process.nextTick(() =>
 
-        // To keep the example simple, the user's GitHub profile is returned to
-        // represent the logged-in user.  In a typical application, you would want
-        // to associate the GitHub account with a user record in your database,
-        // and return that user instead.
-        return done(null, profile);
-      });
-    }
-  ));
+      // To keep the example simple, the user's GitHub profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the GitHub account with a user record in your database,
+      // and return that user instead.
+      done(null, profile),
+    );
+  }),
+));
 passport.serializeUser((user, done) => {
   done(null, user);
 });
@@ -86,23 +91,18 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((obj, done) => {
   done(null, obj);
 });
-//NOTE prev code \/
+// NOTE prev code \/
 
 app.use((req, res, next) => {
-  res.locals.user = req.user
-  console.log(res.locals.users, 'res.locals.user');
-  console.log(req.user, 'req.user');
-  console.log(res.session);
-  next()
-})
+  res.locals.user = req.user;
+  // console.log(res.locals.users, 'res.locals.user');
+  // console.log(req.user, 'req.user');
+  // console.log(res.session);
+  next();
+});
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  rootValue: root,
-  graphiql: true,
-}));
 
-//NOTE prev code /\
+// NOTE prev code /\
 
 // NOTES WITH daniel  -- need session keys generated
 // CORS headers ALLOWCross Domain
@@ -118,19 +118,24 @@ app.use('/graphql', graphqlHTTP({
 // passport.authenticate('github', { scope: [ 'user:email', 'read:org', 'notifications' ] }),
 
 app.get('/auth/github',
-  passport.authenticate('github', { scope: [ 'user:email', 'read:org', 'notifications' ] }));
+  passport.authenticate('github', { scope: ['user:email', 'read:org', 'notifications', 'repo'] }));
 
 app.get('/auth/github/callback',
   passport.authenticate('github'),
   (req, res) => {
-    console.log('passport.authenticate', req.session.passport);
-    return res.json(req.session.passport)
+    return res.json(req.session.passport);
   });
 
+  app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  }));
+
 // NOTE logout triggers end of session pass new session info to DB
-app.get('/logout', function(req, res){
+app.get('/logout', (req, res) => {
   req.logout();
-  res.status(200).json(JSON.stringify('logout'))
+  res.status(200).json(JSON.stringify('logout'));
 });
 
 
@@ -148,5 +153,5 @@ if (!module.parent) {
 }
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
+  res.redirect('/login');
 }
