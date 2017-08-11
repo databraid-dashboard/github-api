3c467bfba63eecc40c042a7f4042af7659b4686f
Grant Willison <emailgdw@gmail.com>
2017-08-10 19:04:12 -0700

    Added some validation and isAuthenticated route, currently broken

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const methodOverride = require('method-override');
 
 const app = express();
 app.use(partials());
-app.use(methodOverride());
+// app.use(methodOverride());
 
 const PORT = process.env.PORT || 8000;
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
@@ -43,19 +43,6 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-
-passport.use(new GitHubStrategy({
-  clientID: GITHUB_CLIENT_ID,
-  clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: 'http://localhost:8000/auth/github/callback',
-},
-  ((accessToken, refreshToken, profile, done) => {
-    process.env.TKN = accessToken;
-    process.nextTick(() =>
-      done(null, profile),
-    );
-  }),
-));
 passport.serializeUser((user, done) => {
   done(null, user);
 });
@@ -63,19 +50,65 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((obj, done) => {
   done(null, obj);
 });
-// NOTE prev code \/
+
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://localhost:8000/auth/github/callback',
+},
+  ((accessToken, refreshToken, profile, done) => {
+    console.log(accessToken, 'round 1');
+    console.log(process.env.TKN, "TKN 1");
+    process.env.TKN = accessToken;
+    console.log(accessToken, 'round 2');
+    console.log(process.env.TKN, "TKN 2");
+    process.nextTick(() =>
+      done(null, profile),
+    );
+  }),
+));
 
 app.use((req, res, next) => {
   res.locals.user = req.user;
   next();
 });
+app.get('/isAuthenticated', (req, res, next) => {
+  console.log(req.isAuthenticated(), 'is authenticated??');
 
+  if (req.isAuthenticated()) {
+    res.status(201).send({isAuth: 'true'})
+  } else {
+    res.status(404).send({isAuth: 'false'})
+  };
+
+})
 app.get('/auth/github',
-  passport.authenticate('github', { scope: ['user:email', 'read:org', 'notifications', 'repo'] }));
+  passport.authenticate('github', { scope: ['user:email', 'read:org', 'notifications', 'repo'] })
+);
 
 app.get('/auth/github/callback',
-  passport.authenticate('github'),
-  (req, res) => res.json(req.session.passport));
+  passport.authenticate('github', { failureRedirect: '/ghj' }),
+
+  (req, res) => {
+    // console.log('here!', req.session.passport.user._json.organizations_url);
+  // res.cookie({userName : req.session.passport.user._json.login, userOrgs: req.session.passport.user._json.organizations_url, httpOnly: false})
+  // res.cookie({userName: req.session.passport.user._json.login})
+  // console.log(`The userName is: ${req.session.passport.user._json.login} and the userOrgs URL is: ${req.session.passport.user._json.organizations_url}, httpOnly:${}`);
+  res.cookie('userName', req.session.passport.user._json.login, {
+    httpOnly: false
+  })
+  res.cookie('userOrgsURL', req.session.passport.user._json.organizations_url,  {
+    httpOnly: false
+  })
+
+  res.redirect('http://localhost:3000/') //res.json(req.session.passport.user)
+  }
+);
+
+
+
+//NOTE new route for req.session.passport.user._json.login
+
 
 app.use('/graphql', graphqlHTTP({
   schema,
@@ -94,6 +127,7 @@ app.get('/logout', (req, res) => {
 app.use((req, res) => {
   res.sendStatus(404);
 });
+
 
 
 if (!module.parent) {
