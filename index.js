571b746d19174fb3939fbc9ad7a6f428c333e06b
Grant Willison <emailgdw@gmail.com>
2017-08-24 15:20:22 -0700

    Add Env variable assignments for 'development'

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ passport.deserializeUser((obj, done) => {
 passport.use(new GitHubStrategy({
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: NODE_ENV === 'development'? LOCAL_CALLBACK_URL : 'https:deployed_site.com',
+  callbackURL: NODE_ENV === 'development' ? LOCAL_CALLBACK_URL : 'https:deployed_site.com',
 },
   ((accessToken, refreshToken, profile, done) => {
     process.env.TKN = accessToken;
@@ -89,7 +89,7 @@ passport.authenticate('github', { failureRedirect: '/' }),
     httpOnly: false
   })
 
-  res.redirect(urlPath)
+  res.redirect(urlPath())
 }
 );
 
