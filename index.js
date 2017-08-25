ff3470c5132842f06c32d57be33dcf5558394e23
Grant <emailgdw@gmail.com>
2017-08-25 13:57:44 -0700

    Fixed Conflicts (#8)
    *  testing dev branch remote
    
    * Add OAuth routes to server, user.locals is undefined
    
    * Finish OAuth routes, Passing back proper response to req.session.passport
    
    * Complete Oauth, must lint code
    
    * Clean up linting errors, ready for PR
    
    * Add some validation and isAuthenticated route, currently broken
    
    * Oauth hookup in progress
    
    * Complete OAuth strategy
    
    * Clean up commit messages
    
    * Add npm install to Dockerfile instructions
    
    * Resolve changes as directed minus app.use on line 57
    
    * Add health check route handler
    
    * Reserve deployment steps for master branch commits only
    
    * Remove express middleware to store user from req to res
    
    * Add Env variable assignments for 'development'
    
    * Add Env variable assignments for 'development'
    
    * Remove package-lock
    
    * Add cors handler and move health check middleware
    
    * Refactor env variable assignments
    
    * Fix linting

--- index.js
+++ index.js
@@ -1,15 +1,68 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHTTP = require('express-graphql');
-const cors = require('cors');
 const root = require('./src/resolvers/RootResolver');
 const schema = require('./src/schema/schema');
+const GitHubStrategy = require('passport-github2').Strategy;
+const passport = require('passport');
+const partials = require('express-partials');
+const session = require('express-session');
+const cors = require('cors');
 
 const app = express();
+
 const PORT = process.env.PORT || 8000;
 
-app.use(bodyParser.json());
+function urlPath() {
+  if (process.env.NODE_ENV === 'development') {
+    return 'http://localhost:3000/';
+  }
+  return 'http://someLiveURL.com/';
+}
+
+app.use(partials());
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.SECRET,
+  keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2],
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: process.env.NODE_ENV === 'development' ? process.env.LOCAL_CALLBACK_URL : 'https:deployed_site.com',
+},
+  ((accessToken, refreshToken, profile, done) => {
+    process.env.TKN = accessToken;
+    process.nextTick(() =>
+      done(null, profile),
+    );
+  }),
+));
+
 
 app.use(
   '/graphql',
@@ -21,11 +74,27 @@ app.use(
 );
 
 
-// This handler is for the health checking from the load balancer
+app.get('/auth/github',
+  passport.authenticate('github', { scope: ['user:email', 'read:org', 'notifications', 'repo'] }),
+);
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/' }),
+  /* eslint-disable no-underscore-dangle */
+  (req, res) => {
+    res.cookie('userName', req.session.passport.user._json.login, {
+      httpOnly: false,
+    });
+    res.cookie('isAuth', 'true', {
+      httpOnly: false,
+    });
+
+    res.redirect(urlPath());
+  },
+);
 app.use('/', (req, res) => {
   res.sendStatus(200);
 });
-
 app.use((req, res) => {
   res.sendStatus(404);
 });
